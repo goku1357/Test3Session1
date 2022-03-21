792a07357cff6894af874f4eac252533547c3db0
goku1357 <shrishaabysani@gmail.com>
2022-03-21 16:27:34 +0530

    p1

--- p1original.c
+++ p1original.c
@@ -1,2 +1,39 @@
+#include<stdio.h>
+void input(int *a, int *b, int *c, int *d)
+{
+  scanf("%d",a);
+  scanf("%d",b);
+  scanf("%d",c);
+  scanf("%d",d);
+  
+}
+int add(int a,int b, int c, int d, int *n, int *dd)
+{
 
+  *n=(a*d)+(b*c);
+  *dd=(b*d);
+  int gcd;
+  for(int i=1;i<=*n && i<=*dd;++i)
+    {
+      if(*n%i==0 && *dd%i==0)
+      {
+        gcd=i;
+      }
+    }
+  return gcd;
+}
+void output(int n,int dd,int gcd)
+{
+  printf("the answer is %d / %d",n/gcd,dd/gcd);
+  
+}
+
+int main()
+{
+  int a,b,c,d,n,dd;
+  input(&a,&b,&c,&d);
+  int gcd;
+  gcd=add(a,b,c,d,&n,&dd);
+  output(n,dd,gcd);
+ 
 }
